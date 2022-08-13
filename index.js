89668b43802780fb0326c6cab46a86bef4a4f98f
Mark Bran <47259119+markytobran@users.noreply.github.com>
2022-08-13 17:34:22 +0100

    Adds new config settings

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const app = require('./app')
+const app = require('./src/app')
 const dotenv = require('dotenv')
 dotenv.config({ path: '../config.env' })
-const port = process.env.PORT || 80
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`)
