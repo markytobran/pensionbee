674c41c16f72ca15a925f1a04518fd47d8d34781
Mark Bran <47259119+markytobran@users.noreply.github.com>
2022-08-13 17:18:13 +0100

    Changed port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 const dotenv = require('dotenv')
 dotenv.config({ path: '../config.env' })
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 80
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`)
